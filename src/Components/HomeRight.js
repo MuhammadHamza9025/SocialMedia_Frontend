8cdba78fdf7725ca705cea323e780e7270936c3c
MuhammadHamza9025 <mhamza9025@gmail.com>
2024-07-24 11:06:04 +0500

    update

--- src/Components/HomeRight.js
+++ src/Components/HomeRight.js
@@ -25,24 +25,26 @@ const HomeRight = () => {
                 <h2 className='my-10 font-bold text-lg text-center'>Add to your Friend List</h2>
 
                 {/*  */}
-                {
-                    allusers.map((e) => {
-                        return (
-                            <>
+                <div className='border flex flex-row sm:flex-col'>
+                    {
+                        allusers.map((e) => {
+                            return (
+                                <>
 
-                                <Link to={`/users/${e._id}`}>
-                                    <div className='flex cursor-pointer '>
-                                        <img src={e.image || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ISC9UU8aZv3dBBEAc23t4mzgdkPLVgpdk2ClCRapGw&s"} alt="" className='h-[50px]  rounded-full border border-gray-500 w-[50px] m-4' />                                        <div className='block mt-2'>
-                                            <p className='text-sm font-semibold font-sans'>{e.name}</p>
-                                            <p className='text-xs'>{e.name.toLowerCase().replace(/ /g, '')}</p>
+                                    <Link to={`/users/${e._id}`}>
+                                        <div className='flex cursor-pointer sm:flex-row flex-col items-center justify-center p-2 m-4'>
+                                            <img src={e.image || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ISC9UU8aZv3dBBEAc23t4mzgdkPLVgpdk2ClCRapGw&s"} alt="" className='h-[50px]  rounded-full border border-gray-500 w-[50px] sm:m-4' />                                        <div className='block mt-2'>
+                                                <p className='text-sm font-semibold font-sans'>{e.name}</p>
+                                                <p className='text-xs'>{e.name.toLowerCase().replace(/ /g, '')}</p>
+                                            </div>
+
                                         </div>
-
-                                    </div>
-                                </Link>
-                            </>
-                        )
-                    })
-                }
+                                    </Link>
+                                </>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </>
     )
--- src/Components/MainHome.js
+++ src/Components/MainHome.js
@@ -120,7 +120,7 @@ const MainHome = ({ data }) => {
 
     return (
         <>
-            <div className='w-[49%] max-h-[100%]'>
+            <div className='sm:w-[49%] max-h-[100%]'>
                 {
                     posts.map((item, index) => {
 
